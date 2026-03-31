0c4d8226f4da86cf71b3aeb572ee06318c726ab9
iektm1-03 <iektm1-03@ek2irakl.lan>
2026-03-31 16:07:13 +0300

    ndryshimet

--- Documents/HTMLCSSJAVASPHP/JAVAPHP/php1/pages/page10.php
+++ Documents/HTMLCSSJAVASPHP/JAVAPHP/php1/pages/page10.php
@@ -2,5 +2,4 @@
 include_once 'functions.php';
 showRecords("SELECT * FROM categories", 2);
 showRecords("SELECT * FROM products", 3);
-showRecords("SELECT * FROM customers", 2);
-?>
+showRecords("SELECT * FROM users", 4);

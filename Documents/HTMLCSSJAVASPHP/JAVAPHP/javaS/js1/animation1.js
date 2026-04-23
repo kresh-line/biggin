27e7d3804a70423082480ecb8bd46ebf7c2e3626
iektm1-03 <iektm1-03@ek2irakl.lan>
2026-04-23 17:43:38 +0300

    ndryshimet

--- Documents/HTMLCSSJAVASPHP/JAVAPHP/javaS/js1/animation1.js
+++ Documents/HTMLCSSJAVASPHP/JAVAPHP/javaS/js1/animation1.js
@@ -1,19 +1,19 @@
-var movdiv = document.getElementById('movdiv');
-var pmovdiv = document.getElementById('parentmovdiv');
+var movdiv2 = document.getElementById('movdiv2');
+var pmovdiv2= document.getElementById('parentmovdiv2');
 
-var topval = pmovdiv.offsetHeight;
-var stepval=-5;
-function moveDiv(){
-    movdiv.style.top = topval + "px";
-    topval+=stepval;
-
+var topval2 = pmovdiv2.offsetHeight;
+var stepval2=-2;
+function moveDiv2(){
+    movdiv2.style.top = topval2 + "px";
+    topval2+=stepval2;
+    
     // αω το τοπ γινει μικροτερο απο το αρνητικο υψος του movdiv τοτε βαλε το τοπ 
     // στο υψος του pmovdiv
-    if (topval<-movdiv.offsetHeight){
-        topval = pmovdiv.offsetHeight;
+    if (topval2<-movdiv2.offsetHeight){
+        topval2 = pmovdiv2.offsetHeight;
     }
-    setTimeout(moveDiv,100);
+    setTimeout(moveDiv2,33);
 }
 
-//setInterval(moveDiv, 100);
-moveDiv();
+//setInterval(moveDiv2, 100);
+moveDiv2();

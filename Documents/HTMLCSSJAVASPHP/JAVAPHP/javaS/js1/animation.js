3e2ff70c90afe8cedcc114b12533341617079d5d
iektm1-03 <iektm1-03@ek2irakl.lan>
2026-04-23 16:04:31 +0300

    ndryshimet

--- Documents/HTMLCSSJAVASPHP/JAVAPHP/javaS/js1/animation.js
+++ Documents/HTMLCSSJAVASPHP/JAVAPHP/javaS/js1/animation.js
@@ -1,39 +1,25 @@
-/*var movdiv = document.getElementById('movdiv');
-var pmovdiv = document.getElementById('parentmovdiv');
-
-var topval = pmovdiv.offsetHeight;
-var stepval=-5;
-function moveDiv(){
-    movdiv.style.top = topval + "px";
-    topval+=stepval;
-
-    // αω το τοπ γινει μικροτερο απο το αρνητικο υψος του movdiv τοτε βαλε το τοπ 
-    // στο υψος του pmovdiv
-    if (topval<-movdiv.offsetHeight){
-        topval = pmovdiv.offsetHeight;
-    }
-    setTimeout(moveDiv,100);
-}
-
-//setInterval(moveDiv, 100);
-moveDiv();*/
 
 var movdiv = document.getElementById('movdiv');
 var pmovdiv = document.getElementById('parentmovdiv');
-
 var topval = pmovdiv.offsetHeight;
-var stepval=-5;
-function moveDiv(){
-    movdiv.style.top = topval + "px";
-    topval+=stepval;
+const stepval=-2;
+var lasttime = -1;
+var FPS = 30;
+var FRAMEINTERVAL = 1000/FPS;
 
-    // αω το τοπ γινει μικροτερο απο το αρνητικο υψος του movdiv τοτε βαλε το τοπ 
-    // στο υψος του pmovdiv
-    if (topval<-movdiv.offsetHeight){
-        topval = pmovdiv.offsetHeight;
+function moveDiv(executiontime){
+    // αν ειναι η πρωτη φορα που τρεχει η συναρτηση ή αν εχουν περασει 33ms απο την τελευταια φορα που τρεξε
+    // 33ms γιατι θελουμε να τρεχει με 30fps (1000ms/30=33.3ms)
+    if (executiontime-lasttime>=FRAMEINTERVAL || lasttime==-1){
+        movdiv.style.top = topval + "px";
+        topval+=stepval;
+        if (topval < -movdiv.offsetHeight){
+            topval = pmovdiv.offsetHeight;
+        }
+        lasttime = executiontime;
     }
-    setTimeout(moveDiv,100);
+    //Zηταμε απο το browser να τρεξει την moveDiv στο επομενο frame
+    requestAnimationFrame(moveDiv);
 }
 
-//setInterval(moveDiv, 100);
-moveDiv();
+requestAnimationFrame(moveDiv);
---
+++ Documents/HTMLCSSJAVASPHP/JAVAPHP/javaS/js1/animation1.js
@@ -0,0 +1,19 @@
+var movdiv = document.getElementById('movdiv');
+var pmovdiv = document.getElementById('parentmovdiv');
+
+var topval = pmovdiv.offsetHeight;
+var stepval=-5;
+function moveDiv(){
+    movdiv.style.top = topval + "px";
+    topval+=stepval;
+
+    // αω το τοπ γινει μικροτερο απο το αρνητικο υψος του movdiv τοτε βαλε το τοπ 
+    // στο υψος του pmovdiv
+    if (topval<-movdiv.offsetHeight){
+        topval = pmovdiv.offsetHeight;
+    }
+    setTimeout(moveDiv,100);
+}
+
+//setInterval(moveDiv, 100);
+moveDiv();

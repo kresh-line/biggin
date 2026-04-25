63588fc6a7c4fbc37f8aebae2982e6146c2b2997
kresh-line <merushehazizi82@outlook.com>
2026-04-25 15:38:26 +0000

    cody

---
+++ python/BFS_queue.py
@@ -0,0 +1,18 @@
+def person_is_seller(name):
+    return name[-1]=='m'
+def search(name):
+    search_queue=deque()
+    search_queue+=graph[name]
+    searched=set()
+    while search_queue:
+        person=search_queue.popleft()
+        if not person in searched:
+            if person_is_seller(person):
+
+             print(person + "is a mango seller!")
+            return True
+        else:
+             search_queue+=graph[person]
+             searched.add(person)
+    return False                
+search("you")

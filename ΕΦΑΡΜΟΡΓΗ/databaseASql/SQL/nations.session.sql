11761bd551744b53fbeb3902226461b005a8d2a3
Niku-haz <merushehazizi82@outlook.com>
2026-04-30 02:03:57 +0300

    hjhgf

--- ΕΦΑΡΜΟΡΓΗ/databaseASql/SQL/nations.session.sql
+++ ΕΦΑΡΜΟΡΓΗ/databaseASql/SQL/nations.session.sql
@@ -1,2 +1,12 @@
-SELECT * FROM countries;
+
 select count(*) from country_languages;
+
+select languages. language from languages , country_languages
+,countries
+where languages. language_id=country_languages. language_id
+and country_languages. country_id= countries. country_id and
+countries. name like 'South Africa';
+
+SELECT name
+FROM countries
+WHERE area = (SELECT MIN(area) FROM countries);

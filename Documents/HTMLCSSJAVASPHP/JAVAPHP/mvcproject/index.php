d9808ca6acf6c280079cea70263b1ba906b0f930
Niku-haz <merushehazizi82@outlook.com>
2026-05-05 13:06:33 +0300

    fds

--- Documents/HTMLCSSJAVASPHP/JAVAPHP/mvcproject/index.php
+++ Documents/HTMLCSSJAVASPHP/JAVAPHP/mvcproject/index.php
@@ -1,7 +1,7 @@
 <?php
         //Φύλαξε το folder path της web εφαρμογής σου
 	$site_path = realpath(dirname(__FILE__));
-	//chdir($site_path);
+		//chdir($site_path);
 
 	require_once('application/router.php');
 

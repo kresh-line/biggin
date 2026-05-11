bb1e0b264f6e716add0b76496330de6566c07000
Niku-haz <merushehazizi82@outlook.com>
2026-05-11 21:52:23 +0300

    fdd

--- ΕΦΑΡΜΟΡΓΗ/databaseASql/SQL/products.sql
+++ ΕΦΑΡΜΟΡΓΗ/databaseASql/SQL/products.sql
@@ -6,8 +6,8 @@ USE shop;
 CREATE TABLE IF NOT EXISTS products (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    price DECIMAL(10,2) NOT NULL,
-    description VARCHAR(255)
+    description VARCHAR(255),
+    price DECIMAL(10,2) NOT NULL
 );
 
 -- Shto produkte

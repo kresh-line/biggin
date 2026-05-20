5724766013e097bcd8c529d6f06d7a78df944701
Niku-haz <merushehazizi82@outlook.com>
2026-05-20 22:46:24 +0300

    hgf

--- Documents/HTMLCSSJAVASPHP/JAVAPHP/javaS/js1/forms.js
+++ Documents/HTMLCSSJAVASPHP/JAVAPHP/javaS/js1/forms.js
@@ -1,4 +1,4 @@
-var f = document.getElementById("formtocheck");
+var f = document.getElementById("formtocheck1");
 
 f.addEventListener("submit", checkSubmission);
 
@@ -36,26 +36,12 @@ function checkSubmission(e) {
         msgstr = msgstr + "Παρακαλώ επιλέξτε τουλάχιστον 2 επιλογές.<br>";
     }
 
-// let num = 0;
-
-    if (f1.elements['choice1'].checked === true) 
-        num++;
-    
-    if (f1.elements['choice2'].checked === true) 
-        num++;
-    
-    if (f1.elements['choice3'].checked === true) 
-        num++;
-    
-    if (num < 2) {
-        msgstr = msgstr + "Παρακαλώ επιλέξτε τουλάχιστον 2 επιλογές.<br>";
-    }
-
-
+    //e.preventDefault(); // gjithmonë ndal refresh-in e faqes
     if (msgstr !== "") {
-        errorform1.innerHTML = msgstr;
-        e.preventDefault();
-    }
+        errormgs.innerHTML = msgstr;
+    } //else {
+        //errormgs.innerHTML = "U dërgua me sukses!"; // nuk ka gabime
+    //}
 }
 
 /*f.elements['fruit'].addEventListener("input", checkFruit);
@@ -80,11 +66,32 @@ f1.addEventListener("submit", checkform1);
 var errorform1 = document.getElementById("errorform1");
 function checkform1(e) {
     let msgstr = "";
-    if (f.elements['email'].value.endsWith("@gmail.com") === false) {
+    if (f1.elements['fruit'].checkValidity() === false) {
+        if (f1.elements['fruit'].validity.patternMismatch === true) {
+            msgstr = msgstr + "Παρακαλώ επιλέξτε μπανάνα ή κεράσι.<br>";
+        }
+        if (f1.elements['fruit'].validity.valueMissing === true) {
+            msgstr = msgstr + "Παρακαλώ επιλέξτε ένα φρούτο.<br>";
+        }
+    }
+    if (f1.elements['email'].checkValidity() === false) {
+        msgstr = msgstr + "bale to email.<br>";
+    } else if (f1.elements['email'].value.endsWith("@gmail.com") === false) {
         msgstr = msgstr + "be email mono sto @gmail.com.<br>";
     }
+    let num = 0;
+    if (f1.elements['choice1'].checked === true) num++;
+    if (f1.elements['choice2'].checked === true) num++;
+    if (f1.elements['choice3'].checked === true) num++;
+    if (num < 2) {
+        msgstr = msgstr + "Παρακαλώ επιλέξτε τουλάχιστον 2 επιλογές.<br>";
+    }
+    //e.preventDefault();
     if (msgstr !== "") {
         errorform1.innerHTML = msgstr;
         e.preventDefault();
     }
+    //else {
+    //    errorform1.innerHTML = "U dërgua me sukses!";
+    //}
 }

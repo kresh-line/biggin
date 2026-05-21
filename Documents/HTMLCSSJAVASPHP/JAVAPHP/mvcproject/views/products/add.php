3388ca70e231a8d7a2db5fd57e8b047c3ce56ba5
iektm1-03 <iektm1-03@ek2irakl.lan>
2026-05-21 15:15:52 +0300

    ndryshimet

--- Documents/HTMLCSSJAVASPHP/JAVAPHP/mvcproject/views/products/add.php
+++ Documents/HTMLCSSJAVASPHP/JAVAPHP/mvcproject/views/products/add.php
@@ -3,7 +3,34 @@
 ?>
     <div id="content">
         <h1>Εισαγωγή Προϊόντος</h1>
+        <div class='productsformdiv'>\n";
+            <form id='insertProductForm'  method='post'>
+                <fieldset>
+        
+            <label for='pname'>Όνομα</label>
+            <input type='text' id='pname' name='pname'  placeholder="Όνομα Προϊόντος">
+            <label for='pprice'>Τιμή</label>
+            <input type='number' id='pprice' name='pprice' min="0.00" max="9999.99" step="0.01" value='0.00' placeholder="Τιμή Προϊόντος">
+
+            <label for='pcat$r[0]'>Κατηγορία</label>
+            <select id='pcat$r[0]' name='pcat'>
+            <?php
+            /*
+            for ($j=0; $j<count($categ); ++$j) {
+               
+               echo "<option value='" . $categ[$j][0] . "'>" . $categ[$j][1] . "</option>\n";
+            }
+           */ 
+            ?>
+            </select>
+
+            <br><button class='insertButton' type='submit'>Εισαγωγή</button>
+            </fieldset>
+ 
+            </form>
+        </div>
     </div>
+        
 <?php 
     require_once("views/footer.php"); 
 ?>	

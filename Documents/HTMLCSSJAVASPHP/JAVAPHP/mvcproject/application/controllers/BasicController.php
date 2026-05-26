b88037847e313da7cf2d734d949430e91e259f3b
iektm1-03 <iektm1-03@ek2irakl.lan>
2026-05-26 16:40:45 +0300

    ndryshimet

--- Documents/HTMLCSSJAVASPHP/JAVAPHP/mvcproject/application/controllers/BasicController.php
+++ Documents/HTMLCSSJAVASPHP/JAVAPHP/mvcproject/application/controllers/BasicController.php
@@ -28,16 +28,18 @@ class BasicController {
             $pwd1 = $_POST["pswd"];
             $pwd2 = $_POST["pswd2"];
             $bd = $_POST["bd"];
-            if (filter_var($username, FILTER_VALIDATE_EMAIL) !=true ||str_ends_with($username, "@gmail.com") == false) {
-               
-               
-             $msg = $msg . "<h3>Το email πρέπει να είναι έγκυρο και να τελειώνει σε @gmail.com</h3>";
-               
-            }
+            if (filter_var($username, FILTER_VALIDATE_EMAIL) !=true ||str_ends_with($username, "@gmail.com") == false) 
+               $msg = $msg . "<h3>Το email πρέπει να είναι έγκυρο και να τελειώνει σε @gmail.com</h3>";
+            
+            if ($pwd1 != $pwd2) 
+                $msg = $msg . "<h3>πρέπει οι κωδικοί πρόσβασης να ταιριάζουν</h3>";
+            if (strlen($pwd1) < 8 || strlen($pwd1) > 30)
+                $msg = $msg . "<h3>Ο κωδικός πρόσβασης πρέπει να είναι τουλάχιστον 8 χαρακτήρες και το πολύ 30</h3>";
             //Εδώ θα έπρεπε να κάνω έλεγχο για το αν τα
             $GLOBALS["registerMsg"] = $msg;
         }
         
         require_once($GLOBALS['viewDir'] . '/register.php'); 
     }
+    
 }

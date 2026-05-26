48c93d54eee87fedd1974be79b4c01a816871c85
iektm1-03 <iektm1-03@ek2irakl.lan>
2026-05-26 16:03:47 +0300

    ndryshimet

--- Documents/HTMLCSSJAVASPHP/JAVAPHP/mvcproject/application/controllers/BasicController.php
+++ Documents/HTMLCSSJAVASPHP/JAVAPHP/mvcproject/application/controllers/BasicController.php
@@ -20,15 +20,24 @@ class BasicController {
         require_once($GLOBALS['viewDir'] . '/info.php'); 
     }
     function register() {
+        $msg = "";
+        $GLOBALS["registerMsg"] = "";
+        //Ελέγχω αν έχω υποβληθεί η φόρμα
         if (isset($_POST["user"]) ) {
-            //Εδώ θα έπρεπε να κάνω έλεγχο για το αν τα πεδία είναι σωστά συμπληρωμένα
-            //και μετά να κάνω την εγγραφή στη βάση δεδομένων
-            //Αλλά για την ώρα απλά θα εμφανίζω τα στοιχεία που έστειλε ο χρήστης
-            $GLOBALS["registerMsg"] = "<h3>Εγγραφή με τα εξής στοιχεία:</h3>";
+            $username = $_POST["user"];
+            $pwd1 = $_POST["pswd"];
+            $pwd2 = $_POST["pswd2"];
+            $bd = $_POST["bd"];
+            if (filter_var($username, FILTER_VALIDATE_EMAIL) !=true ||str_ends_with($username, "@gmail.com") == false) {
+               
+               
+             $msg = $msg . "<h3>Το email πρέπει να είναι έγκυρο και να τελειώνει σε @gmail.com</h3>";
+               
+            }
+            //Εδώ θα έπρεπε να κάνω έλεγχο για το αν τα
+            $GLOBALS["registerMsg"] = $msg;
         }
-        else {
-            $GLOBALS["registerMsg"] = "<h3>Παρακαλώ συμπληρώστε τα στοιχεία εγγραφής</h3>";
-        }
+        
         require_once($GLOBALS['viewDir'] . '/register.php'); 
     }
 }

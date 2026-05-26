e679845f54aed7ed0f3fa5daa1aa0d1714f9fb61
unknown <iektm1-03@ek2irakl.lan>
2026-05-26 14:44:36 +0300

    ndryshimet

--- c programm/c++/incherent/polimorphism1.cpp
+++ c programm/c++/incherent/polimorphism1.cpp
@@ -1,72 +1,72 @@
 #include <iostream>
 using namespace std;
-class Shape {
-    protected:
-        int width, height;
-    public:
-    Shape( int a = 0, int b = 0){
+class Shape
+{
+protected:
+    int width, height;
+
+public:
+    Shape(int a = 0, int b = 0)
+    {
         width = a;
         height = b;
     }
-   virtual void print() {
-        cout << "Shape object" << endl;
-        cout << "Area = " << area() << endl;
-    }
-    virtual int area() {
-        return 0;
-    };
-};
-class Rectangle: public Shape {
-    public:
-    Rectangle( int a = 0, int b = 0):Shape(a, b) { }
-    int area() {
-        return width*height;
-    };
+    virtual void print() = 0; // pure virtual function
     
-    void print() {
+    virtual int area() = 0; // pure virtual function
+    
+};
+class Rectangle : public Shape
+{
+public:
+    Rectangle(int a = 0, int b = 0) : Shape(a, b) {}
+    int area()
+    {
+        return width * height;
+    };
+
+    void print()
+    {
         cout << " Rectangle object" << endl;
         cout << "Area = " << area() << endl;
     }
-     
-    friend ostream& operator<<(ostream &ostr, Rectangle &p);
-  
-    
- };
 
-    ostream& operator<<(ostream &ostr, Rectangle &p){ 
-      return ostr <<"rectangle: " << p.width <<"x" << p.height << endl;
-    } 
+    friend ostream &operator<<(ostream &ostr, Rectangle &p);
+};
 
+ostream &operator<<(ostream &ostr, Rectangle &p)
+{
+    return ostr << "rectangle: " << p.width << "x" << p.height << endl;
+}
 
-class Triangle: public Shape {
-    public:
-    Triangle( int a = 0, int b = 0):Shape(a, b) { }
-     int area() {
-        return (width*height)/2;
+class Triangle : public Shape
+{
+public:
+    Triangle(int a = 0, int b = 0) : Shape(a, b) {}
+    int area()
+    {
+        return (width * height) / 2;
     };
-    
-    void print() {
+
+    void print()
+    {
         cout << "Trangle object " << endl;
         cout << "Area = " << area() << endl;
     }
-  
 };
 
-int main () {
+int main()
+{
     Shape *s;
 
-    
-    Rectangle rect(10,7);
+    Rectangle rect(10, 7);
     cout << rect;
     s = &rect;
     s->print();
-    Triangle tria(10,5);
+    Triangle tria(10, 5);
     s = &tria;
     s->print();
 
-    Shape myshape(3,5);
    
-    myshape.print();
-    
     return 0;
 }

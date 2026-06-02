6a04eb77c35ac2a0f79c9aafdb3593bcb4be4cf6
iektm1-03 <iektm1-03@ek2irakl.lan>
2026-06-02 17:06:05 +0300

    ndryshimet

---
+++ Documents/HTMLCSSJAVASPHP/JAVAPHP/mvcproject/views/ajaxget.php
@@ -0,0 +1,23 @@
+<?php 
+    require_once("views/header.php"); 
+?>
+    <div id="content">
+        <h1> φορμα Υπολογισμού - Χωρις AJAX</h1>
+        <form action="" method="post">
+            <fieldset>
+                <legend>Προσθεσι</legend>
+            <label for="num1">Αριθμός 1:</label>
+            <input type="number" id="num1" name="num1" step="1" value="0" required><br>
+            
+            <label for="num2">Αριθμός 2:</label>
+            <input type="number" id="num2" name="num2" step="1" value="0" required><br>
+            
+            <button type="submit">Υπολογισμός</button>
+            </fieldset>
+        </form>
+        <span id="spanformresult" class='formresult'>  </span>   
+    </div>
+<?php 
+    require_once("views/footer.php"); 
+?>	
+

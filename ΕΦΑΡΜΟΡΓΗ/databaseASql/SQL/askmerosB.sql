0cb332c3feb1ba162159abbbe1a0be9500600a50
unknown <iektm1-03@ek2irakl.lan>
2026-06-10 16:59:37 +0300

    ndryshimet

---
+++ ΕΦΑΡΜΟΡΓΗ/databaseASql/SQL/askmerosB.sql
@@ -0,0 +1,21 @@
+create table Kathghths 
+(
+    KK int primary key,
+    Ονομα varchar(255) not null,
+    Επονυμο varchar(255) not null,
+    Μισθος decimal(10, 2) not null
+);
+
+create table Mathimata
+(
+    Cid int primary key,
+    Τιτλος varchar(255) not null,
+    KK int not null,
+    foreign key (KK) references Kathghths(KK)
+
+);
+
+
+INSERT INTO Kathghths VALUES (1, 'Nikos', 'Papadopoulos', 2500.00), (2, 'Maria', 'Konstantinou', 2800.00); 
+INSERT INTO Mathimata VALUES ('CS101', 'Baseis Dedomenwn', 1), ('CS102', 'Programmatismos', 2); 
+

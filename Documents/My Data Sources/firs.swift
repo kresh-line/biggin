8c9118b844e2859b0ac3bdbcd8538c7d7b0b5cf9
Niku-haz <merushehazizi82@outlook.com>
2026-06-10 20:56:25 +0300

    bv

---
+++ Documents/My Data Sources/firs.swift
@@ -0,0 +1,11 @@
+var emri = "Krenar"
+let mbiem = "hazid"
+var mosha = 40
+print("Unë jam \(emri) \(mbiem)")
+if (mosha>=25) {
+  print("mosha është thjesht një numër.")
+  }
+else {
+  print("jam i ri akoma")
+}  
+

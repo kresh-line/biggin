ead52cbd2ba472b1a095a71a8e37a9414553cbf6
iektm1-03 <iektm1-03@ek2irakl.lan>
2026-06-11 17:21:13 +0300

    ndryshimet

--- Documents/HTMLCSSJAVASPHP/JAVAPHP/mvcproject/views/manual.php
+++ Documents/HTMLCSSJAVASPHP/JAVAPHP/mvcproject/views/manual.php
@@ -8,7 +8,9 @@
         <?php
         $f = scandir("docs/");
         for ($i=2; $i < count($f); ++$i)
-            echo "<a href='docs/" . $f[$i] . "'>" . $f[$i] . "</a><br>";
+            $n = "/mvcproject/docs/" . $f[$i];
+
+            echo "<a href='$n' target='_blank'> $f[$i]  </a><br>";
 
         ?>
     </div>
